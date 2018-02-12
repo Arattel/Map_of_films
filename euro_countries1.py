8390385782282fd5c5dfc408276ed10ff91afd2e
Syzonov Oleksandrgit config --global user.name  Syzonov <syzonov@ucu.edu.ua>
2018-02-12 22:04:24 +0200

    Added main module and additional with countries

---
+++ euro_countries1.py
@@ -0,0 +1,6 @@
+countries = ['Europe', 'Македонија', 'Andorra', 'Colombia', 'Österreich', 'Deutschland', "Nederland", 'België / Belgique / Belgien', 'Бълга̀рия', 'Беларусь',
+             'Česko', ' Europe', 'Danmark', 'Eesti', 'Suomi', ' France', 'Ελλάδα', 'Magyarország', 'Ireland', 'UK',
+             'Italia', 'Liechtenstein', 'Lietuva', 'België / Belgique / Belgien', 'Latvija', 'Malta', 'Philippines',
+             'Norge', 'RP', 'Portugal', 'România', 'РФ','Sverige', 'Slovenija', 'Slovensko', 'San Marino',
+             'Україна', 'Civitas Vaticana','BiH / БиХ', 'Hrvatska', 'Moldova', 'Monaco', ' Brasil', 'Србија', 'España',
+             'Schweiz/Suisse/Svizzera/Svizra', ' الإمارات العربيّة المتّحدة']
---
+++ map_creator_1.py
@@ -0,0 +1,134 @@
+import folium
+from geopy import Nominatim
+from euro_countries1 import  countries
+
+
+def create_map():
+    """
+      -> None
+    This function inputs a year of film creation and maximum amount of films on a map and creates file with
+    map with films
+    """
+    year, number_coordinates = input_year_and_place()
+def input_year_and_place():
+    """
+    ->(int, int)
+    This function returns number of places and year
+    """
+    year = int(input())
+    number_coordinates = int(input())
+    return (year, number_coordinates)
+def process_film(film):
+    """
+    (str) -> (tuple)
+    This function processes film and returns it in view of tuple (year, film, place)
+    """
+    film = film.split()
+    name = ''
+    year = ''
+    episode = ''
+    place = ''
+    name = film[0]
+    del film[0]
+    if name.count('"') == 1:
+        while name.count('"') != 2:
+            name += ' ' + film[0]
+            del film[0]
+    year = film[0][1:-1]
+    del film[0]
+    if '{' not in film[0]:
+        episode = '-'
+    else:
+        episode = film[0]
+        del film[0]
+        if '{' in episode and '}' not in episode:
+            while '{' in episode and '}' not in episode:
+                episode += ' ' + film[0]
+                del film[0]
+        episode = episode[1:-1]
+    place = ' '.join(film)
+    if '(' in place and ')' in place:
+        while '(' in place:
+            place = place[:-1]
+        place = place[:-1]
+    return (year, name, place)
+def search_by_year(year, number_coordinates):
+    """
+    (int, int) -> list((tuple(str, str)))
+    This function inputs year and number of coordinates and outputs list of films with given year with place and
+    in list not longer then number of coordinates
+    """
+    year = str(year)
+    film_list = []
+    with open('locations.list', 'r', encoding='utf-8', errors='ignore') as data:
+        read = False
+        for line in data:
+            if read:
+                if len(film_list) >= number_coordinates:
+                    break
+                else:
+                    film = line
+                    if film:
+                        film = process_film(line)
+                        if film[0] == year:
+                            film_list.append(film[1:])
+            elif '==============' in line:
+                read = True
+    return  film_list
+def define_area(country):
+    """
+    (str) -> (str)
+    This function outputs 'Europe' if country is european, 'USA" if country is 'United States of America" else
+    'other'
+    """
+    if country in countries or country[1:] in countries:
+        return "Europe"
+    elif country in ' United States of America':
+        return "USA"
+    else:
+        return "other"
+def find_coordinates(place):
+    """
+    (str) -> (tuple)
+    This function inputs name of place and returns tuple with it`s coordinates
+    """
+    geolocator = Nominatim()
+    location = geolocator.geocode(place, timeout=138)
+    if location:
+        return (location.latitude, location.longitude, location.raw['display_name'].split(',')[-1])
+def add_coordinates_and_area(film_list):
+    """
+    (list) -> (dict)
+    This function inputs coordinates and outputs it as a dictionary with region as key and dictiondary with keys
+    'name' and 'coordinates'
+    """
+    film_list_1 = []
+    for i, j  in enumerate(film_list):
+        name = j[0]
+        place = find_coordinates(j[1])
+        if place:
+            film_list_1.append((define_area(place[2]), name, place[:2]))
+    film_dict = {}
+    for i in film_list_1:
+        if i[0] in film_dict:
+            film_dict[i[0]].append({'name':i[1], 'coordinates':i[2]})
+        else:
+            film_dict[i[0]] = [{'name':i[1], 'coordinates':i[2]}]
+    return film_dict
+def save_map(film_dict):
+    """
+    (dict) -> None
+    This function inputs film dictionary and saves map in file
+    """
+    map_films = folium.Map()
+    for i in film_dict:
+        group = folium.FeatureGroup(name = i)
+        for j in film_dict[i]:
+            group.add_child(folium.Marker(list(j['coordinates']), popup=j['name'],
+                                              icon=folium.Icon(color='blue', icon='info-sign')))
+        map_films.add_child(group)
+    map_films.add_child(folium.LayerControl())
+    map_films.save('Map.html')
+
+
+print(save_map(add_coordinates_and_area(search_by_year(2015, 250))))
